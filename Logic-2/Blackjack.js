aa1e800fdaf73bb45c049fc8a6ccd6c5920aa12d
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-07-07 15:06:10 -0400

    new problem selected for Jenesh peer programming

---
+++ Logic-2/Blackjack.js
@@ -0,0 +1,26 @@
+// Logic-2 -- blackjack
+// https://codingjs.wmcicompsci.ca/exercise.html?name=blackjack&title=Logic-2
+
+// Peer Coded with Jenesh Napit, Software Developer Apprentice at Codecademy!
+// FOLLOW JENESH!
+// LINKEDIN: https://www.linkedin.com/in/jeneshnapit/
+// COMPANY LINKEDIN: https://www.linkedin.com/company/mypantry/
+
+
+// Given 2 int values greater than 0, return whichever value is nearest to 21 
+// without going over. Return 0 if they both go over.
+
+// Examples
+
+// blackjack(19, 21) → 21
+// blackjack(21, 19) → 21
+// blackjack(19, 22) → 19
+
+const blackjack = (a,b) => {
+  
+};
+
+console.log(blackjack(19, 22));
+console.log(blackjack(21, 19));
+console.log(blackjack(19, 22));
+console.log(blackjack(23, 25));
--- Logic-2/MakeBricks.js
+++ Logic-2/MakeBricks.js
@@ -1,12 +1,6 @@
 // Logic-2 -- makeBricks
 // https://codingjs.wmcicompsci.ca/exercise.html?name=makeBricks&title=Logic-2
 
-// Peer Coded with Jenesh Napit, Software Developer Apprentice at Codecademy!
-// FOLLOW JENESH!
-// LINKEDIN: https://www.linkedin.com/in/jeneshnapit/
-// COMPANY LINKEDIN: https://www.linkedin.com/company/mypantry/
-
-
 // We want to make a row of bricks that is goal inches long. 
 // We have a number of small bricks (1 inch each) and big bricks 
 // (5 inches each). Return true if it is possible to make the goal 
@@ -21,14 +15,9 @@
 // makeBricks(3, 2, 10) → true
 
 const makeBricks = (small,big,goal) => {
-  let smallBrick = small;
-  let bigBrick = big * 5;
-  if (smallBrick + bigBrick === goal) {
-      return true;
-  } else {
-      return false;
-  }
+
 };
 
 console.log(makeBricks(3, 1, 8));
 console.log(makeBricks(3, 1, 9));
+console.log(makeBricks(3, 2, 10))
