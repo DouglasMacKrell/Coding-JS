d4ac86895fe65aca45096f47020fb268d6bda3d7
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-07-16 14:55:59 -0400

    SumDouble complete

---
+++ Warmup-1/SumDouble.js
@@ -0,0 +1,26 @@
+// Warmup-1 -- sumDouble
+// https://codingjs.wmcicompsci.ca/exercise.html?name=sumDouble&title=Warmup-1
+
+// Given two int values, return their sum. 
+// Unless the two values are the same, 
+// then return double their sum.
+
+// Examples
+
+// sumDouble(1, 2) → 3
+// sumDouble(3, 2) → 5
+// sumDouble(2, 2) → 8
+
+const sumDouble = (a, b) => {
+    if (a === b) {
+        return (a * 2) + (b * 2);
+    } else {
+        return a + b;
+    }
+}
+
+console.log(sumDouble(1, 2)); // 3
+console.log(sumDouble(3, 2)); // 5
+console.log(sumDouble(2, 2)); // 8
+console.log(sumDouble(5, 5)); // 20
+
