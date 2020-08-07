2f688781a7929e9c9af3365acfa2e66eb8ee0582
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-07 18:16:37 -0400

    Diff21 solved

---
+++ Warmup-1/DIff21.js
@@ -0,0 +1,16 @@
+// Warmup-1 -- diff21
+// https://codingjs.wmcicompsci.ca/exercise.html?name=diff21&title=Warmup-1
+
+// Given an int n, return the absolute difference between 
+// n and 21, except return double the absolute difference 
+// if n is over 21.
+
+// Examples
+
+// diff21(19) → 2
+// diff21(10) → 11
+// diff21(21) → 0
+
+const diff21 = (n) => {
+    return 21 >= n ? 21 - n : (n - 21) * 2;
+}
