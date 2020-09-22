79f572f68a1ad2ff9a3ce2c227c261ceb434dbce
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-22 19:13:16 -0400

    MixString complete from section String-2

---
+++ String-2/MixString.js
@@ -0,0 +1,37 @@
+// String-2 -- mixString
+// https://codingjs.wmcicompsci.ca/exercise.html?name=mixString&title=String-2
+
+// Given two strings, a and b, create a bigger string
+// made of the first char of a, the first char of b,
+// the second char of a, the second char of b, and so on.
+// Any leftover chars go at the end of the result.
+
+// Examples
+
+// mixString('abc', 'xyz') → axbycz
+// mixString('Hi', 'There') → HTihere
+// mixString('xxxx', 'There') → xTxhxexre
+
+function mixString(a, b) {
+  let stack = [];
+  let arrA = a.split("");
+  let arrB = b.split("");
+  let stackCount = arrA.length + arrB.length
+  stack.push(arrA.shift());
+  const blender = (arr1, arr2) => {
+    if (stack.length === stackCount) {
+      return;
+    }
+    if (arrB.length !== 0) {
+        stack.push(arr2.shift());
+    } 
+    if (arrA.length !== 0) {
+        stack.push(arr1.shift());
+    }
+    blender(arr1, arr2)
+  };
+  blender(arrA, arrB)
+  return stack.join("")
+}
+
+console.log(mixString("abc", "xyz"));
