dfd86e2929cdf57f87029e2632f83f09290cf77b
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-27 17:38:49 -0400

    codeCounter from String-2 complete

--- String-2/CountCode.js
+++ String-2/CountCode.js
@@ -1,8 +1,23 @@
-String-2 -- countCode
-Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+// String-2 -- countCode
+// https://codingjs.wmcicompsci.ca/exercise.html?name=countCode&title=String-2
 
-Examples
+// Return the number of times that the string "code"
+// appears anywhere in the given string,
+// except we'll accept any letter for the 'd',
+// so "cope" and "cooe" count.
 
-countCode('aaacodebbb') → 1
-countCode('codexxcode') → 2
-countCode('cozexxcope') → 2
+// Examples
+
+// countCode('aaacodebbb') → 1
+// countCode('codexxcode') → 2
+// countCode('cozexxcope') → 2
+
+function countCode(str) {
+  let codeCounter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "c" && str[i + 1] === "o" && str[i + 3] === "e") {
+      codeCounter += 1;
+    }
+  }
+  return codeCounter;
+}
