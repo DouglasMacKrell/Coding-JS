a4b2fa888cbf094af9455982d5ea2fcbfae213f0
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-10-05 18:52:36 -0400

    stringSplosion from Warmup-2 complete

---
+++ Warmup-2/StringSplosion.js
@@ -0,0 +1,18 @@
+// Warmup-2 -- stringSplosion
+// https://codingjs.wmcicompsci.ca/exercise.html?name=stringSplosion&title=Warmup-2
+
+// Given a non-empty string like "Code"
+// return a string like "CCoCodCode".
+
+// Examples
+
+function stringSplosion(str) {
+  let splodeStr = str.split("");
+  let result = "";
+  for (let i = 0; i < splodeStr.length; i++) {
+    result += splodeStr.slice(0, i + 1).join("");
+  }
+  return result;
+}
+
+console.log(stringSplosion("happenstance"));
