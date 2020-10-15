f38013b0ec1f961109f7c9e34954edf3a8e9c8e8
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-10-14 20:27:13 -0400

    sameFirstLast from Array-1 complete

---
+++ Array-1/SameFirstLast.js
@@ -0,0 +1,21 @@
+// Array-1 -- sameFirstLast
+// https://codingjs.wmcicompsci.ca/exercise.html?name=sameFirstLast&title=Array-1
+
+// Given an array of ints, return true if
+// the array is length 1 or more, and the
+// first element and the last element are
+// equal.
+
+// Examples
+
+// sameFirstLast([1, 2, 3]) → false
+// sameFirstLast([1, 2, 3, 1]) → true
+// sameFirstLast([1, 2, 1]) → true
+
+function sameFirstLast(nums) {
+  if (nums.length >= 1 && nums[0] === nums[nums.length - 1]) {
+    return true;
+  } else {
+    return false;
+  }
+}
