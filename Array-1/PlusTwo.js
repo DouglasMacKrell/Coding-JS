c14c76db89eeed3cf195302e9655ea6b13505429
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-10-14 20:35:23 -0400

    plusTow from Array-1 complete

---
+++ Array-1/PlusTwo.js
@@ -0,0 +1,17 @@
+// Array-1 -- plusTwo
+// https://codingjs.wmcicompsci.ca/exercise.html?name=plusTwo&title=Array-1
+
+// Given 2 int arrays, each length 2, return
+// a new array length 4 containing all their
+// elements.
+
+// Examples
+
+// plusTwo([1, 2], [3, 4]) → 1,2,3,4
+// plusTwo([4, 4], [2, 2]) → 4,4,2,2
+// plusTwo([9, 2], [3, 4]) → 9,2,3,4
+
+function plusTwo(a, b) {
+  let result = [a[0], a[1], b[0], b[1]];
+  return result;
+}
