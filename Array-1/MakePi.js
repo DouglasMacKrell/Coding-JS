ef7146741b4939fb08df912de941e75b09a3d6f9
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-10-15 18:50:08 -0400

    makePi from Array-1 complete

---
+++ Array-1/MakePi.js
@@ -0,0 +1,13 @@
+// Array-1 -- makePi
+// https://codingjs.wmcicompsci.ca/exercise.html?name=makePi&title=Array-1
+
+// Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+// Examples
+
+// makePi() → 3,1,4
+
+function makePi() {
+  let pi = [3, 1, 4];
+  return pi;
+}
