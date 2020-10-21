d9995f8defd4b75d9666181f5351769cb0bbe521
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-10-21 19:38:16 -0400

    start1 from Array-1 complete

---
+++ Array-1/Start1.js
@@ -0,0 +1,22 @@
+// Array-1 -- start1
+// https://codingjs.wmcicompsci.ca/exercise.html?name=start1&title=Array-1
+
+// Start with 2 int arrays, a and b, of any length.
+// Return how many of the arrays have 1 as their first element.
+
+// Examples
+
+// start1([1, 2, 3], [1, 3]) → 2
+// start1([7, 2, 3], [1]) → 1
+// start1([1, 2], []) → 1
+
+function start1(a, b) {
+  let count = 0;
+  if (a[0] === 1) {
+    count += 1;
+  }
+  if (b[0] === 1) {
+    count += 1;
+  }
+  return count;
+}
