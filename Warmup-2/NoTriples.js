d32f3f509987566f434acd70e87399ac762b058b
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-04 22:00:33 -0500

    updated noTriples

--- Warmup-2/NoTriples.js
+++ Warmup-2/NoTriples.js
@@ -15,20 +15,14 @@ function noTriples(nums) {
   if (nums.length < 3) {
     return true;
   }
-  let myObj = {};
   for (let i = 0; i < nums.length; i++) {
-    if (myObj[nums[i]]) {
-      myObj[nums[i]] += 1;
-    } else {
-      myObj[nums[i]] = 1;
+    if (nums[i + 2]) {
+      if (nums[i] === nums[i + 1] && nums[i] === nums[i + 2]) {
+        return false;
+      }
     }
   }
-  for (let num in myObj) {
-    if (myObj[num] === 3) {
-      return true;
-    }
-  }
-  return false;
+  return true;
 }
 
 console.log(noTriples([1, 1, 2, 2, 1]));
