291e797cc62955d6a5972b4a0bd03148c58fb786
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-05 21:59:46 -0500

    helloName from String-1 complete

---
+++ String-1/HelloName.js
@@ -0,0 +1,15 @@
+// String-1 -- helloName
+// https://codingjs.wmcicompsci.ca/exercise.html?name=helloName&title=String-1
+
+// Given a string name, e.g. "Bob", return a greeting
+// of the form "Hello Bob!".
+
+// Examples
+
+// helloName('Bob') → Hello Bob!
+// helloName('Alice') → Hello Alice!
+// helloName('X') → Hello X!
+
+function helloName(name) {
+  return "Hello " + name + "!";
+}
