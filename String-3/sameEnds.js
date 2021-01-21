90985b1a02bd3b7dae3f904cba4cb202cf5e8ab5
DouglasMacKrell <douglasmackrell@pursuit.org>
2021-01-20 20:55:30 -0500

    notReplace from String-3 complete

---
+++ String-3/sameEnds.js
@@ -0,0 +1,15 @@
+// String-3 -- sameEnds
+// https://codingjs.wmcicompsci.ca/exercise.html?name=sameEnds&title=String-3
+
+// Given a string, return the longest substring that appears at both the beginning 
+// and end of the string without overlapping. For example, sameEnds("abXab") is "ab".
+
+// Examples
+
+// sameEnds('abXYab') → true
+// sameEnds('xx') → true
+// sameEnds('xxx') → true
+
+function sameEnds(nums, len) {
+    
+}
