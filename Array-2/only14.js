22c7b0da3e617a47e1fb2e43c284ac6e3c4e283d
DouglasMacKrell <douglasmackrell@pursuit.org>
2021-01-23 18:07:51 -0500

    only14 from Array-2 complete

---
+++ Array-2/only14.js
@@ -0,0 +1,21 @@
+// Array-2 -- only14
+// https://codingjs.wmcicompsci.ca/exercise.html?name=only14&title=Array-2
+
+// Given an array of ints, return true if every element is a 1 or a 4.
+
+// Examples
+
+// only14([1, 4, 1, 4]) → true
+// only14([1, 4, 2, 4]) → false
+// only14([1, 1]) → true
+
+function only14(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1 || nums[i] === 4) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
